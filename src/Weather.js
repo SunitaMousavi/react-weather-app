af0eb52e2b0f9e0bfe82ab7299cffaa9928b9f11
Sunita <sunitamousaviii@gmail.com>
2025-02-16 21:22:18 +0330

    Setup
    Made Weather.js & Weather.css & WeatherInfo.js
    Imported axios & bootstrap
    Cleaned up
    Added footer

---
+++ src/Weather.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import WeatherInfo from "./WeatherInfo";
+import axios from "axios";
+import "./Weather.css";
+
+export default function Weather(props) {
+  return (
+    <div className="Weather">
+      <div className="container">Hello World</div>
+    </div>
+  );
+}
